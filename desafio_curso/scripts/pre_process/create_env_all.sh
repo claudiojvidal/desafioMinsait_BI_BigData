e7454afbb7a00f108b8c4503f97025dd61d3566a
Claudio Vidal <67280710+claudiojvidal@users.noreply.github.com>
2023-06-24 12:38:31 -0300

    .

--- desafio_curso/scripts/pre_process/create_env_all.sh
+++ desafio_curso/scripts/pre_process/create_env_all.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG="${BASEDIR}/../../config/config.sh"
+source "${CONFIG}"
 
-# Criação das pastas
+# Criação das pastas e envio dos arquivos ao HDFS
+echo 'Exportação de arquivos ao HDFS'
+cd ../   /raw/
+for table in "${TABLES[@]}"
+   do   
+      echo "Envio da tabela $table."
+      cd  $table
+      docker exec -it namenode hdfs dfs -mkdir /datalake/raw/$table/
+      docker exec -it namenode hdfs dfs -chmod 777 /datalake/raw/   able/
+      docker exec -it namenode hdfs dfs -copyFromLocal /input/curso_minsait/raw/$table/$table.csv /datalake/raw/$table/    
+      echo "Envio da tabela $table finalizado."
+   done
+cd ../
+echo "Exportação de arquivos ao HDFS em ${DATE} finalizada."
 
-DADOS=("vendas" "clientes" "endereco" "regiao" "divisao")
 
-for i in "${DADOS[@]}"
-do
-	echo "$i"
-    cd ../../raw/
-    hdfs dfs -mkdir /datalake/raw/$i
-    hdfs dfs -chmod 777 /datalake/raw/$i
-    hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
-done
+
